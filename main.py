432b246cab67a0b30b96bb6027d4f12475cfa9aa
Vitor <vitor.dalla213@gmail.com>
2022-06-27 13:38:53 -0300

    Exit method replaced

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from time import sleep
 import os
 from time import sleep
 from pynput import keyboard
-from pynput.keyboard import Listener
+from pynput.keyboard import Listener, Key, Controller
 from threading import Thread, Lock
 import os
 
 arquivo = ''
 stop_all = False
+keyboard = Controller()
+
+def DO_NOTHING():
+    pass
 
 def controles(key):
     if key == keyboard.Key.esc:
@@ -17,16 +21,25 @@ def controles(key):
         global stop_all
         global listener
         stop_all = True
-        exit()
+        listener.stop()
     
     if key == keyboard.Key.right:
         listador.velocidade -= 0.005
-    
     if key == keyboard.Key.left:
         listador.velocidade += 0.005
 
     if key == keyboard.Key.enter:
         listador.next = True
+
+    if key == keyboard.Key.space:
+        if listador.just_stop == True:      
+            listador.just_stop = False
+            print(listador.just_stop)
+
+        else:
+            listador.just_stop == True
+            print(listador.just_stop)
+        return False
 
 class Listador(Thread):
     """
@@ -53,11 +66,13 @@ class Listador(Thread):
             
             
             for word in text:
-                        os.system('clear')
+                        # os.system('clear')
                         print(word.center(50).replace('\n', '')) 
                         print('\n\n\n\n')
-                        print(f'speed: {self.velocidade:.2f} -- \
+                        print(f'Delay: {self.velocidade:.2f} \
                             page: {n}')
+                        print('Press "ESC" to quit'.center(50))
+                        print()
                         if len(word) > 7:
                             sleep(self.velocidade + 0.1)
                         else:
@@ -72,20 +87,20 @@ class Listador(Thread):
                             sleep(0.001)
                             os.system('clear')
                             quit()
-                        
-                        if self.just_stop:
-                            pass
+
+                        while self.just_stop == True:
+                            sleep(0.1)
                             
-                    
                     
 
 if __name__ == '__main__':
 
-    listador = Listador(arquivo)
+    
+    listador = Listador(arquivo) 
     listador.start()
-
-    with Listener(on_press=controles) as listener:
+    with Listener(on_press=controles ) as listener:
         listener.run()
+    
 
 
 
